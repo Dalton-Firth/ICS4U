cec752353b007c35ba3660978d6483b2904b7e6a
Dalton Firth <dalton.firth@student.tdsb.on.ca>
2016-09-13 14:54:39 -0400

    Final version

---
+++ ICS4U/src/firth/NumDigits.java
@@ -0,0 +1,45 @@
+package firth;
+/* NumDigits.java
+ * This program uses recursion to determine the number of digits in a given integer
+ * @author Dalton Firth
+ * @version 2016/09/13
+ */
+
+import java.util.Scanner;
+
+public class NumDigits {
+
+	public static int counter = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("intput an integer");
+
+		int numin = scan.nextInt();
+
+		numDigits(numin);
+
+		// outputting the number of digits in the integer
+		System.out.println("the number " + numin + " has " + counter + " digits");
+		scan.close();
+	}
+
+	/**
+	 * NumDigits counts the number of digit ins a given integer
+	 * 
+	 * @param input
+	 *            the integer given by the user that will have the number of
+	 *            digits it has determined
+	 */
+	public static void numDigits(int input) {
+
+		counter++;
+
+		if (input / 10 != 0)
+			numDigits(input / 10);
+	}
+
+}
